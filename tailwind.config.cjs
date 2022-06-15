8b7f7c499298f694cebd86b02ebf1c23b76c82e0
Nahl Basalamah <nahlultra@gmail.com>
2022-06-15 09:24:15 +0700

    start building index.astro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,25 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'sans': ['rubik', 'ui-sans-serif', 'system-ui', ],
+			'serif': ['rubik', 'ui-serif', 'Georgia', ],
+			'mono': ['ui-monospace', 'SFMono-Regular', ],
+			'display': ['rubik', ],
+			'body': ['rubik', ],
+		},
 		extend: {
 			screens: {
 				sm: '480px',
 				md: '768px',
 				lg: '976px',
 				xl: '1440px',
-			  },
+			},
+			colors : {
+				primary: '#000000',
+				background : '#FFFFFF',
+				outline : '#F6F6F6'
+			}
 		},
 	},
 	plugins: [
